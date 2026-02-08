7e08b5ad9b24a15f99c739e8906f5d3879613aac
isanthosh-dev <isanthoshkumar76@gmail.com>
2026-02-08 19:55:20 +0530

    githubchanges

--- mechanical-mercury/astro.config.mjs
+++ mechanical-mercury/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://isanthoshkumar76-tech.github.io',
+base: '/mechanical-mercury/',
   devToolbar: { enabled: false },
   vite: { plugins: [tailwindcss()] },
   integrations: [react()],
